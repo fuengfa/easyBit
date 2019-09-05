42cced31109a496ede7a6e80fc1e87d516e81b56
fuengfa <fuengfa1996@gmail.com>
2019-09-05 14:08:07 +0700

    first commit

---
+++ app/src/main/java/com/scb/easybit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.scb.easybit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
