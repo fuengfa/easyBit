c09162706644ca4888c2cc3976e1036444ccba69
fuengfa <fuengfa1996@gmail.com>
2019-09-09 13:06:28 +0700

    add cloud firestore

--- app/src/main/java/com/scb/easybit/MainActivity.kt
+++ app/src/main/java/com/scb/easybit/MainActivity.kt
@@ -2,7 +2,11 @@ package com.scb.easybit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.viewpager.widget.ViewPager
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,78 +18,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        imgArray?.add("https://www.xn--12c1bij4d1a0fza6gi5c.com/wp-content/uploads/2019/07/iphone-xr-2019-concept-1280x720.jpg")
         imgArray?.add("https://1.bp.blogspot.com/-x6ar420f2sA/XFmWj_fX2OI/AAAAAAAADiE/X1wdpbJDYkwDDdc-6EkofeneJtAO4vCfQCLcBGAs/s1600/1469069-20190205110021-ba01ac6.jpg")
         imgArray?.add("https://www.appdisqus.com/wp-content/uploads/2018/10/iphone-xr-select-static-201809_GEO_EMEA-1280x720.jpg")
-        imgArray?.add("https://www.xn--12c1bij4d1a0fza6gi5c.com/wp-content/uploads/2019/07/iphone-xr-2019-concept-1280x720.jpg")
 
         imageSlider = findViewById(R.id.viewpager)
-        imageSlider?.adapter = PhotoPagerAdapter(supportFragmentManager,imgArray)
+        imageSlider?.adapter = PhotoPagerAdapter(supportFragmentManager, imgArray)
+
+        val db = FirebaseFirestore.getInstance()
+
+        val bithigh = db.collection("room2/userbit")
+            .get()
 
 
-//        val database = FirebaseDatabase.getInstance()
-//        myRef = database.reference
-//
-//        // Read from the database
-//        myRef.addValueEventListener(object : ValueEventListener {
-//
-//            override fun onDataChange(dataSnapshot: DataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
-//                val map = dataSnapshot.value as Map<*, *>
-//                val value = map["bitroom"].toString()
-//
-//
-//                Toast.makeText(this@MainActivity, "addValueEventListener", Toast.LENGTH_SHORT).show()
-//
-//                hightBit.text = value
-//
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                hightBit.text = error.message
-//            }
-//        })
 
 
-//        val store = findViewById<View>(R.id.bit1) as Button
-//
-//        store.setOnClickListener {
-//                view: View? -> store ()
-//        }
 
+        db.collection("bitroom/room2/userbit").addSnapshotListener { snapshot, e ->
+            if(e != null){
+                Log.w("errorlisten", "listen:error", e)
+                return@addSnapshotListener
+            }
+            if (snapshot != null){
+                for (dc in snapshot.documentChanges){
+                    if (dc.type == DocumentChange.Type.ADDED) {
+                        Log.d("listenSuccess", "New city: ${dc.document.data}")
+                        hightBit.text = "${dc.document.data.get("bitcost")}"
+                        username.text = "${dc.document.data.get("username")}"
+                        money.text = "${dc.document.data.get("bitcost")}"
+                    }
+                }
+            }
+        }
+
+
+        bit1.setOnClickListener {
+            val data1 = mutableMapOf<String,Any>(
+                "bitcost" to "2000",
+                "username" to "fuengfa",
+                "userid" to "3"
+            )
+            val cities = db.collection("bitroom/room2/userbit")
+                .add(data1)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("fuengfa", "DocumentSnapshot written with ID: ${documentReference.id.get(1)}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("fuengfa", "Error adding document", e)
+                }
+        }
     }
-
-//    private fun store () {
-//        val highBit = findViewById<View>(R.id.hightBit) as TextView
-//        val username = findViewById<View>(R.id.username) as TextView
-//        val money = findViewById<View>(R.id.money) as TextView
-//
-//        var high = highBit.text.toString().trim()
-//        val user = username.text.toString().trim()
-//        val mone = money.text.toString().trim()
-//
-//        if (!high.isEmpty() && !user.isEmpty() && !mone.isEmpty()) {
-//            try {
-//                val items = HashMap<String, Any>()
-//                items.put("bit_cost", high)
-//                items.put("user_id", user)
-//                items.put("user_name", mone)
-//                db.collection("bitroom")
-//                    .add(items)
-//                    .addOnSuccessListener { documentReference ->
-//                        Log.d("Hi ", "DocumentSnapshot added with ID: " + documentReference.id)
-//                    }
-//                    .addOnFailureListener { e ->
-//                        Log.w("Hi ", "Error adding document", e)
-//                        //Show Dialog with a failure message
-//                    }
-//            }catch (e:Exception) {
-//                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
-//            }
-//        }else {
-//            Toast.makeText(this, "Please fill up the fields :(", Toast.LENGTH_LONG).show()
-//        }
-//    }
-
-
 }
